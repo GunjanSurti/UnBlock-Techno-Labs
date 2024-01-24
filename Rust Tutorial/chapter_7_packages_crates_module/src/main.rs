b0ffcaad9acd2c567605245594f4a519054eeba3
GunjanSurti <gunjansurti142@gmail.com>
2024-01-24 17:38:37 +0530

    Day 7 ch 7

---
+++ Rust Tutorial/chapter_7_packages_crates_module/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+}
+// when we type "cargo new .. " it creates package 
+// package stores creates
+// crates => 1. binary => code that can be executed (code in main.rs file etc) 
+//           2. library => code that can be used by other programs 

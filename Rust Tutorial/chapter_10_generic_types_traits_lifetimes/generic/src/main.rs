3362a5a3a1f3fc7fd5b696a843230e43e42e08a0
GunjanSurti <gunjansurti142@gmail.com>
2024-01-31 17:32:37 +0530

    Day 12 Generic Type

---
+++ Rust Tutorial/chapter_10_generic_types_traits_lifetimes/generic/src/main.rs
@@ -0,0 +1,75 @@
+fn main() {
+    println!("Hello, Generic!");
+
+    // let char_list = vec!['q', 'e', 'f', 'a'];
+    // let largest = get_largest(char_list);
+    // println!("Largest char = {largest}");
+    // let wont_work = Point { x: 5, y: 4.0 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.a(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+// fn get_largest<T>(list: T) -> &T {
+//     // <T> => can be of any type
+//     let mut largest = &list[0];
+//     for number in list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+//     largest
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn a<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+struct Point<T> {
+    // x and y will be of same type, whatever it is
+    x: T,
+    y: T,
+}
+// can allow more generic types (eg.. T,U,V...)
+struct Point<T, U> {
+    // x and y can be same or different type
+    x: T,
+    y: U,
+}
+
+//********************in enum */
+enum Option<T> {
+    Some(T),
+    None,
+}
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+/*
+Generic type => to reduce code duplication
+
+traits => constrain a generic type to accept only those types that have a particular behavior
+
+Lifetimes => allow us to give the compiler enough information about borrowed values so that it can ensure references will be valid in more situations than it could without our help.
+
+To define the generic "largest function", place type name declarations inside angle brackets, <>, between the name of the function and the parameter list, like this:
+
+
+fn largest<T>(list: &[T]) -> &T {
+
+*/

b243904c243db1954172bc2cdf9c59346764c778
GunjanSurti <gunjansurti142@gmail.com>
2024-02-08 17:35:52 +0530

    Crates

---
+++ Rust Tutorial/chapter_14_cargo_crates_io/src/lib.rs
@@ -0,0 +1,20 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = chapter_14_cargo_crates_io::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+/* Document comment start with "///"
+ * it uses mark down syntax
+ *
+ *  rust can then create html file from your markdown
+ * cargo doc --open
+*/

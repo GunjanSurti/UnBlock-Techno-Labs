9ecc24e0ee17afbcb10159fd4a5eef0d74c6e6df
GunjanSurti <gunjansurti142@gmail.com>
2024-02-19 15:45:34 +0530

    Renaming

--- Rust Tutorial/chapter_19_advanced_features/advanced_traits/src/main.rs
+++ Rust Tutorial/chapter_19_advanced_features/advanced_traits/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+pub trait Iterator {
+    type Item;
+    // this is "associated type"
+    // this is unknown and we define when we implement it
+    // The type Item is a "placeholder"
+    fn next(&mut self) -> Option<Self::Item>;
+
+    // Implementors of the Iterator trait will specify the concrete type for Item, and the next method will return an Option containing a value of that concrete type.
 }
+
+/*
+Difference b/w associated type and generice is that associated type has one concrete type per implementaions
+and generice type has multiple concrete type per implementations
+*/
--- Rust Tutorial/chapter_19_advanced_features/unsafe_rust/src/main.rs
+++ Rust Tutorial/chapter_19_advanced_features/unsafe_rust/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    let address = 0x012345usize;
+    let r = address as *const i32;
+
+    println!("{:#?}", r);
+    println!("{:#?}", address);
+
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3); 
+    // from safe library
+    /* split_at_mut => Divides one mutable slice into two at an index.
+    can take generic too
+    The first will contain all indices from [0, mid) (excluding the index mid itself) and the second will contain all indices from [mid, len) (excluding the index len itself).
+    ******************Panics*****************
+    Panics if "mid > len".
+    */
+
+    println!("{:#?}", a);
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 }

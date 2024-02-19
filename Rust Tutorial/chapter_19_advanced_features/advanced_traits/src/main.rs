405e2c6c5f29efd65ffb0db7e61c276c24e847b6
GunjanSurti <gunjansurti142@gmail.com>
2024-02-19 17:36:59 +0530

    Day 26

--- Rust Tutorial/chapter_19_advanced_features/advanced_traits/src/main.rs
+++ Rust Tutorial/chapter_19_advanced_features/advanced_traits/src/main.rs
@@ -1,5 +1,3 @@
-fn main() {}
-
 pub trait Iterator {
     type Item;
     // this is "associated type"
@@ -8,6 +6,36 @@ pub trait Iterator {
     fn next(&mut self) -> Option<Self::Item>;
 
     // Implementors of the Iterator trait will specify the concrete type for Item, and the next method will return an Option containing a value of that concrete type.
+}
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    // Default Generic Type Parameters and Operator Overloading
+    // here we are overriding Add method so it can add two Points struct
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+}
+
+trait Pilot {
+    fn fly(&self);
 }
 
 /*
--- Rust Tutorial/chapter_19_advanced_features/advanced_types/src/main.rs
+++ Rust Tutorial/chapter_19_advanced_features/advanced_types/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    // Creating Type Synonyms with Type Aliases
+    type Kilometer = i32;
+    let x = 5;
+    let y: Kilometer = 9;
+    // here we have given alias
+
+    println!("x + y = {} + {} ", x, y);
+
+    //The main use case for type synonyms is to "reduce repetition"
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+    // "thunk" is a word for code to be evaluated at a later time, so it’s an appropriate name for a closure that gets stored
+    let _f: Thunk = Box::new(|| println!("hi"));
+
+    fn _takes_long_type(_f: Thunk) {
+        // fn takes_long_type(f: Box<dyn Fn() + Send + 'static>) {
+        // --snip--
+    }
+
+    // fn returns_long_type() -> Thunk {
+    // fn returns_long_type() -> Box<dyn Fn() + Send + 'static> {
+    // --snip--
+    // }
+
+    // dynamically sized type called str
+    // let s1: str = "Hello there!";    
+    // let s2: str = "How's it going?";
+    // both above two sentence will not compile 
 }
+
+// The Never Type that Never Returns
+fn bar() -> ! {
+    // --snip--
+    //the function bar returns never
+    panic!(); // this method does not return anything
+}
+
+
+/*
+the concept of dynamically sized types. Sometimes referred to as "DSTs" or unsized types, these types let us write code using values whose size we can know only at "runtime".
+
+That’s right, not "&str", but "str" on its own, is a DST. We can’t know how long the string is until runtime, meaning we can’t create a variable of type str, nor can we take an argument of type str
+
+*/
